553447f82388dccdc14e267c7442130229de8780
Harsh Thapa <harshthapa4@gmail.com>
2022-09-08 22:52:24 +0530

    Updates

---
+++ precache-manifest.23022d6d637eab74d6a460d7cdc615fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "06cde5c99813b4343f6a",
+    "url": "/monsters-rolodex/static/css/main.334b16ee.chunk.css"
+  },
+  {
+    "revision": "06cde5c99813b4343f6a",
+    "url": "/monsters-rolodex/static/js/main.06cde5c9.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/monsters-rolodex/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/monsters-rolodex/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "baf071cd605113dee954",
+    "url": "/monsters-rolodex/static/js/runtime~main.baf071cd.js"
+  },
+  {
+    "revision": "a4ef801613f972ff93880159c94d46f5",
+    "url": "/monsters-rolodex/index.html"
+  }
+];
